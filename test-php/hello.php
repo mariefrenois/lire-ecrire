3482101070c050d0807182d7a54db722280378ce
marie frenois <marie.am.frenois@live.fr>
2017-04-03 11:19:14 +0200

    Update hello.php

--- test-php/hello.php
+++ test-php/hello.php
@@ -3,10 +3,23 @@
 //pour page d'acceuil
     $db = new PDO('mysql:host=localhost;dbname=lireecrire', 'root', 'root');
     
+    //faire une requête pour récupérer des éléments de la db
+    foreach($db->query('SELECT titre, cover FROM article') as $result){
+        echo '<h1>'.$result['titre'].'</h1>';
+        echo $result['cover'];
+        
+    }
+    
+    $titre = $_POST['titre'];
+    $date = date('Y-m-d');
+    $description =$_POST['description'];
+    $meme = ''; //=$_POST['']; ?
+    $type = 0;
+    
     
 //pour envoie du formulaire
     //faire une requête pour insérer un élément dans la db
-    $requete = $db->prepare('INSERT INTO article (titre, date, description, cover, auteur, mail, liens, ref, type) VALUES (:titre, :date, :description, :cover, :auteur, :mail, :liens, :ref, :type)');
+    $requete = $db->prepare('INSERT INTO article (titre, date, description, cover, auteur, mail, liens, ref, type) VALUES (:titre, :date, :description, :cover, :auteur, :auteurlire, :mail, :liens, :ref, :type, :tags)');
     
     $requete->bindParam(':titre', $titre);
 
@@ -15,28 +28,36 @@
 
     $requete->bindParam(':cover',  $meme);
     $requete->bindParam(':auteur', $meme);
+    $requete->bindParam(':auteurlire', $meme);
     $requete->bindParam(':mail',  $meme);
     $requete->bindParam(':liens',  $meme);
     $requete->bindParam(':ref',  $meme);
     $requete->bindParam(':type', $type);
+    $requete->bindParam(':tags', $type);
     
     try{
         $requete->execute();
     }catch(PDOException $e){
         echo $e;
     }
-        echo 'coucou';
+        echo 'hello';
+
 
 
 
 ?>
 <html>
     <head>
-        <title>Hello World en PHP</title>
+        <title>FLUX</title>
     </head>
     <body>
         <p>
-            <?php echo 'Hello World !'; ?>
+            <?php echo 'FLUX!'; 
+            
+            
+            ?>
+            
+            
         </p>
     </body>
 </html>
